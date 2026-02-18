62fc75a675b06f6f4e696fdf4b4c86449f02dc72
Jose David Gutierrez <a25josedgutalg@iesluiscarrillodesotomayor.org>
2026-02-18 08:25:18 +0100

    Agregar archivos de proyecto.

---
+++ PracticaCV2_JoseDavidGA/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticaCV2_JoseDavidGA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
